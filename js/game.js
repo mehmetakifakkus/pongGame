0e573480d5dcecd872690c8d8516ea314592d8fc
mehmetakifakkus <mehmetakifakkus@gmail.com>
2016-09-27 22:43:08 +0300

    explosion sprite is added. walking man walks faster.

--- js/game.js
+++ js/game.js
@@ -8,8 +8,6 @@ var player = new Player(2, 8, 'assets/sprites/walking.png');
 var text = new PointText({ point: [view.size.width/1.25, 50], justification: 'center', fontSize: 30, strokeColor:'white'});
 var wavyLine;
 
-advanceWavy.mod = 1
-
 /////////////////////////////////   Keyboard Interactions   /////////////////////////////
 
 function readKeys(){    
@@ -27,19 +25,20 @@ function readKeys(){
     {
         if(!wavyLine)
         {
-            Crafty.audio.play("shoot", 1, 0.5)
-            wavyLine = new Raster({ source: 'assets/wavy_line2.png', position: [player.path.position.x + dir * player.path.getInternalBounds().width/2, view.size.height + 110], scaling:0.35 });
-            advanceWavy.mod = 1            
+            Crafty.audio.play("shoot", 1, 0.5)        
+            var pos = player.path.position.x + dir * player.path.getInternalBounds().width/2
+            wavyLine = new WavyFire(pos)
         }
     }
     
 }
 
-///////////////////////////////    Motion  of Balls   ///////////////////////////////
+///////////////////////////////    Classes   ///////////////////////////////
 
 createRandomBalls(1);
 
 function Ball(r, p, v ){
+    var tempObj = this;
 	this.radius = r;
  	this.collisionPath = new Path.Circle({center: p, radius: r, strokeWidth: 1});
 	this.vector = v; //new Point(0.8,0.5)
@@ -60,21 +59,14 @@ function Ball(r, p, v ){
             this.vector *= [-1, 1];
         if(pos.y + radius > view.size.height)
         {
-            pos.y = view.size.height - this.radius;  
-            this.vector *= [1, -1];   
+            this.path.position.y = this.oldState.oldY
+            this.vector = this.oldState.vector * [1, -1]
+            Crafty.audio.play("jump", 1, 0.2)  
         }
         if(pos.y - radius < 0)
             this.vector *= [1, -1];
         
-    };
-    
-    this.iterate = function() {
-        this.checkBoundaries();
-        this.applyGravity();
-
-        this.path.rotate(this.vector.x * 2);
-        this.path.position += this.vector;
-        this.collisionPath.position = this.path.position;
+        this.oldState = {vector: this.vector, oldY: pos.y};
     };
     
     this.react = function(b) {
@@ -95,9 +87,38 @@ function Ball(r, p, v ){
             this.vector = this.vector.normalize(10);
     };
     
+    this.path.onFrame = function() {
+        tempObj.checkBoundaries();
+        tempObj.applyGravity();
+
+        tempObj.path.rotate(tempObj.vector.x * 2);
+        tempObj.path.position += tempObj.vector;
+        tempObj.collisionPath.position = tempObj.path.position;
+        
+        for (var i = 0; i < balls.length; i++)
+            tempObj.react(balls[i])
+    };
+    
     this.remove = function(){
         this.collisionPath.remove();
         this.path.remove();
+    }
+    
+    this.divideBall = function(){
+    
+        var r = tempObj.radius;   
+        var pos = tempObj.path.position;
+
+        if(r < 16){
+            tempObj.remove(); 
+            return;
+        }
+
+        var vector1 = new Point({ angle: 245, length: 8});
+        var vector2 = new Point({ angle: 305, length: 8});
+
+        balls.push(new Ball(3*r/5, pos + [-3*r/5,0], vector1));    
+        balls.push(new Ball(3*r/5, pos + [3*r/5, 0], vector2));    
     }
 };
 
@@ -132,7 +153,6 @@ function Player(row, col, str){
                 fr[0].visible = true;
             }
             temp.loaded = 1;
-            console.log(fr[0])
             temp.path = fr[0];
         });
         return fr;
@@ -159,7 +179,64 @@ function Player(row, col, str){
         i++   
     };    
 };
+
+function WavyFire(x){
     
+    var amount = 30; // amount of the segments  // 
+    this.selected = true
+    var height = 6; // The maximum height of the wave:
+
+    this.wavyFire = new Path({strokeColor: 'blue', strokeWidth: 6, strokeCap: 'square' });
+    for (var i = 0; i <= 30; i++) {
+        this.wavyFire.add(new Point(0, view.size.height + 2*i*(view.size.width/3) / 60));
+    }
+
+    this.tip = new Path({fillColor: 'blue', closed:true,  visible:false});
+    this.tip.add(new Point(0,0), new Point(-8, -12), new Point(15, 0), new Point(-8, 12));
+
+    this.tip2 = new Path()
+    
+    var temp = this;
+    this.wavyFire.onFrame = function(event) {
+        // Loop through the segments of the path:
+        for (var i = 0; i <= 30; i++) {
+            var sinus = Math.sin(-event.time * 30+i); // A cylic value between -1 and 1
+            this.segments[i].point.x = x + sinus * height; // Change the y position of the segment point:
+            this.segments[i].point.y -= 5 // advance forward
+        }
+        this.smooth();
+
+        var p1 = this.segments[0].point;
+        var p2 = this.segments[1].point;
+
+        var degree = (180/Math.PI) * Math.atan2(p1.y - p2.y, p1.x - p2.x);
+        temp.tip.position = this.segments[0].point;
+
+        temp.tip2.remove()
+        temp.tip2 = temp.tip.clone()
+        temp.tip2.visible = true
+        temp.tip2.rotate(degree)
+
+        if(this.segments[0].point.y < 200)
+        {
+            this.remove()
+            temp.tip.remove()
+            temp.tip2.remove()
+            wavyLine = 0;
+        }
+    }
+    
+    this.remove = function()
+    {
+        temp.tip.remove()
+        temp.tip2.remove()
+        this.wavyFire.remove();
+        wavyLine = 0;
+    }
+}
+
+///////////////////////////////////////// Initalize /////////////////////////////////
+
 function createRandomBalls(count){
     for (var i = 0; i < count; i++) {
         var position = Point.random() * (view.size-400) + 60;
@@ -167,33 +244,6 @@ function createRandomBalls(count){
         var radius = Math.random() * 20 + 40;
         balls.push(new Ball(radius, position, vector));
     }
-}
-
-function drawBall() {
-	for (var i = 0; i < balls.length; i++)
-	{
-		balls[i].iterate();
-		for (var j = 0; j < balls.length; j++)
-			if (i != j)
-                balls[i].react(balls[j]);
-	}
-} 
-
-function divideBalls(ball){
-    
-    var r = ball.radius;   
-    if(r < 16)
-    {
-        ball.remove(); return;
-    }
-    var pos = ball.path.position;
-    
-    var vector1 = new Point({ angle: 245, length: 8});
-    var vector2 = new Point({ angle: 305, length: 8});
-    
-    balls.push(new Ball(3*r/5, pos + [-3*r/5,0], vector1));    
-    balls.push(new Ball(3*r/5, pos + [3*r/5, 0], vector2));    
-
 }
 
 ///////////////////////////////    Collision etc   ///////////////////////////////
@@ -219,62 +269,95 @@ function checkCollision(){
     for (var i = 0; i < balls.length; i++) // wavyline
 	{
         if(wavyLine)
-        if(wavyLine.intersects(balls[i].collisionPath))
+        if(wavyLine.wavyFire.intersects(balls[i].collisionPath) || wavyLine.tip.intersects(balls[i].collisionPath))
         {
-            Crafty.audio.play("explosion", 1, 0.25)   
-
-            wavyLine.remove()
-            wavyLine = 0;
-            
-            divideBalls(balls[i]);
-            balls[i].remove()
-            balls.splice(i,1)
-            
             score+= 5;
             document.getElementById("score").innerHTML = "Score: "+ score;
+            Crafty.audio.play("explosion", 1, 0.75)   
+            readSprite(4, 4, 'assets/sprites/explosion_sprite.png', balls[i].path.position, 20)
+ 
+            wavyLine.remove();
+            balls[i].divideBall();
+            balls[i].remove()
+            balls.splice(i,1)
         }
     }
 }
 
-function advanceWavy(){
-    if(wavyLine)
-    {
-        if(advanceWavy.mod == 1)
+/////////////Sprite
+
+function readSprite(row, col, str, pos, speed){
+    
+    item = new Raster({ source: str, visible: false});
+    
+    if(typeof frames_ != 'undefined'){
+        var size = frames_.length;
+        for(var i=size-1; i>=0; i--)
         {
-            wavyLine.position.y -= 10;
-            if(wavyLine.position.y < 385)
-                advanceWavy.mod = 0
+            frames_[i].remove()
+           frames_.pop()
         }
-        if(advanceWavy.mod == 0)
-            wavyLine.position.y += 7;
-        if( wavyLine.position.y > view.size.height + wavyLine.getInternalBounds().height * 0.25)
-            wavyLine = 0
     }
+       
+    var timer = 0
+    frames_ = []
     
+    item.on('load', function(){
+        
+        w = item.getInternalBounds().width
+        h = item.getInternalBounds().height
+
+        xlen = w/col;
+        ylen = h/row;
+        
+        for (var j = 0; j < row; j++) {
+            for(var i = 0; i < col; i++) {
+                r = new Rectangle(xlen*i, ylen*j, xlen, ylen);
+                dd = item.getSubRaster(r);
+                dd.visible = false;
+                dd.position = pos;
+                frames_.push(dd)
+            }
+        }
+    });
+        
+    this.advance = function(){
+        
+            if(advance.i == frames_.length)  
+            {
+                advance.i = 0
+                clearInterval(timer) // comment if you want it endless
+                return
+            }
+
+            if(advance.i == 0)
+                frames_[frames_.length-1].visible = false
+            else
+                frames_[advance.i-1].visible = false;
+            frames_[advance.i].visible = true; 
+            
+        advance.i++         
+    }
+    this.advance.i = 0
+    
+    clearInterval(timer);
+    timer = setInterval(this.advance, 1000/speed);
 }
 
-function draw(){
-    
+function onFrame(event){
     if(!gamePlay)
-       ;// return;
-    
-    drawBall();
-    checkCollision();
-    advanceWavy();
-    
+       ;// return;  
+  
     readKeys();
+    checkCollision();        
 }
 
-setInterval(draw, 33);
-function onFrame(event){}
-
-
-//// load sounds
+//////////////////////////// load sounds
 var assets = {
     'audio': 
         {'beep': 'assets/sounds/beep-04.mp3',
         'shoot': 'assets/sounds/laser_shoot.wav',
-        'jump': 'assets/sounds/jump.wav',
+        'jump': 'assets/sounds/jump9.wav',
         'pulse': 'assets/sounds/pulse.mp3',
         'explosion': 'assets/sounds/explosion.wav',
         'victory': 'assets/sounds/victory.mp3'}
